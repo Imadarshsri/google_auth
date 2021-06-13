5258874ff6e10a2ceca0c970410ed8e68db0484e
Adarsh Srivastava <imadarshsri@gmail.com>
2021-06-14 01:13:45 +0530

    Creates `Google Auth using Passport.js\'

--- auth.js
+++ auth.js
@@ -14,7 +14,7 @@ passport.deserializeUser(function (obj, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback"
+    callbackURL: process.env.CALLBACK_URL,
   },
   function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
