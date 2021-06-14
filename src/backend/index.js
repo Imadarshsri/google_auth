40485ea377f09b254aaf42c16f83e1d08e6a28d9
Adarsh Srivastava <imadarshsri@gmail.com>
2021-06-14 13:20:22 +0530

    Changes the UI and Refactors code

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const hbs = require("hbs");
+const path = require("path");
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
@@ -10,7 +12,11 @@ const port = process.env.PORT || 5000;
 //Configure Passport
 app.use(passport.initialize());
 app.use(passport.session());
-app.set('view engine', 'ejs');
+/// Adds json decoding
+app.use(express.json());
+
+
+// app.set('view engine', 'ejs');
 
 app.use(session({
   resave: true,
@@ -18,13 +24,34 @@ app.use(session({
   secret: 'SECRET'
 }));
 
-app.get('/', function (req, res) {
-  res.render('pages/auth');
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../frontend/public");
+const viewsPath = path.join(__dirname, "../frontend/templates/views");
+const partialsPath = path.join(__dirname, "../frontend/templates/partials");
+
+// Setup handlebars engine & views Location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static to serve
+app.use(express.static(publicDirectoryPath));
+
+// app.get('/', function (req, res) {
+//   res.render('pages/auth');
+// });
+
+app.get("", (req, res) => {
+  res.render("auth", {
+    title: "Google Auth OAuth2.0 Cloud Service",
+    name: "Adarsh Srivastava",
+  });
 });
+
 
 app.get('/success', (req, res) => {
   console.log("Success", req.session.passport.user);
-  res.render('pages/success', {
+  res.render('success', {
     user: req.session.passport.user
   });
 });
@@ -46,11 +73,19 @@ app.get('/auth/google/callback',
     res.redirect('/success');
   });
 
-//todo: Logout
 app.get('/logout', (req, res) => {
   req.session = null;
   req.logout();
   res.redirect('/');
 })
+
+// 404 Error
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404 Error",
+    name: "Adarsh Srivastava",
+    errorMessage: "Page Not Found!",
+  });
+});
 
 app.listen(port, () => console.log('App listening on port ' + port));
